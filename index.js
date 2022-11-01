b0ae49db2bd31dbadac579499a68241e66a6d7d3
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-01 16:23:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+  const winRecord = record.find((element) => element.result === "W");
+  if (winRecord) {
+    return winRecord.year;
+  }
+
+  return undefined;
+}
